eecf2e684ea0dcf533bdb8e0ba6f86874ba21031
Nikita Lapshin <nikita.lapshin2000@gmail.com>
2021-04-11 23:30:34 +0300

    generated data for people table

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql')
+var faker = require('faker');
 
 const conn = mysql.createConnection({
     host: "localhost", 
@@ -16,7 +17,19 @@ conn.connect(function (err) {
     }
 });
 
-let query="SELECT * FROM employee";
+// for(let i =0; i < 100000; i++){
+//     let day = faker.date.between(1958, 2000)
+//     let q = `insert into people (id, name, surname, sex, date, citizenship)
+//       values(${i+1}, "${faker.name.firstName()}", "${faker.name.lastName()}", "${Math.random() > 0.5 ? "Male": "Female"}", "${day.getFullYear()}-${day.getMonth()+1}-${day.getDate()}", "${faker.address.country()}");`
+// conn.query(q , (err, result, field) =>{
+//     console.log(err);
+//     console.log(result);
+//      console.log(field);
+// });
+// }
+
+
+let query="SELECT * FROM people";
 
 conn.query(query, (err, result, field) =>{
     console.log(err);
