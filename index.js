bf9265ed1719e8b16c0feb67f955b15c8d0dfb78
Nikita Lapshin <nikita.lapshin2000@gmail.com>
2021-04-15 03:08:55 +0300

    final version

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const exphbs = require('express-handlebars')
 const homeRoutes = require('./routes/home')
 const zapros_2Routes = require('./routes/zapros_2')
 const zapros_3Routes = require('./routes/zapros_2-2')
+const zapros_4Routes = require('./routes/zapros_4')
+const zapros_5Routes = require('./routes/zapros_5')
 const app = express()
 const hbs = exphbs.create({
     defaultLayout: 'main',
@@ -20,6 +22,8 @@ app.use(express.urlencoded({extended: true}))
 app.use('/', homeRoutes)
 app.use('/zapros_1', zapros_2Routes)
 app.use('/zapros_3', zapros_3Routes)
+app.use('/zapros_4', zapros_4Routes)
+app.use('/zapros_5', zapros_5Routes)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=>{
--- routes/zapros_4.js
+++ routes/zapros_4.js
@@ -1,50 +1,49 @@
-// const {Router} = require('express')
-// const router = Router()
-// const path = require('path')
-// const mysql = require('mysql')
-// const faker = require('faker');
+const {Router} = require('express')
+const router = Router()
+const path = require('path')
+const mysql = require('mysql')
+const faker = require('faker');
 
 
-// // function getRandomArbitrary(min, max) {
-// //     return Math.random() * (max - min) + min;
-// //   }
+// function getRandomArbitrary(min, max) {
+//     return Math.random() * (max - min) + min;
+//   }
 
 
 
-// // select  b.date `dateB`, d.date `dateD`, b.id
-// // from born b
-// // inner join death d on d.id = b.id
-// // where d.date > "2000-01-01" and b.date > "2000-01-01"
+// select  b.date `dateB`, d.date `dateD`, b.id
+// from born b
+// inner join death d on d.id = b.id
+// where d.date > "2000-01-01" and b.date > "2000-01-01"
 
-// router.get('/', (req,res) =>{
-//     const conn = mysql.createConnection({
-//         host: "localhost", 
-//         user: "root",
-//         database: "mydb",
-//         password: "Bmstu1830"
-//     });
-//     conn.connect(function (err) {
-//         if (err) {
-//             return console.error("Ошибка: " + err.message);
-//         }
-//         else {
-//             console.log("Подключение к серверу MySQL успешно установлено");
-//         }
-//     });
-//     let query= `select  con.employeeId \`empCon\`, c.employeId \`empC\`, e.specialId, p.name, p.sex
-//     from contract con
-//     inner join ceremony c on c.employeId = con.employeeId
-//     inner join employee e on e.employeeId = c.employeId
-//     inner join people p on e.specialId = p.id`
-//     conn.query(query, (err, result, field) =>{
-//     console.log(err);
-//     console.log(result);
-//     res.render('/zapros_3', {
-//         result: result,
-//         isZapros3: true,
-//         title: 'zapros_3'
-//     })
-// })
-// conn.end()
-// })
-// module.exports = router
+router.get('/', (req,res) =>{
+    const conn = mysql.createConnection({
+        host: "localhost", 
+        user: "root",
+        database: "mydb",
+        password: "Bmstu1830"
+    });
+    conn.connect(function (err) {
+        if (err) {
+            return console.error("Ошибка: " + err.message);
+        }
+        else {
+            console.log("Подключение к серверу MySQL успешно установлено");
+        }
+    });
+    let query= `select b.PoB, c.place, c.date, b.date, c.id
+    from born b 
+    join ceremony c on b.date = c.date or b.PoB = c.place
+   order by c.id`
+    conn.query(query, (err, result, field) =>{
+    console.log(err);
+    console.log(result);
+    res.render('zapros_3', {
+        result: result,
+        isZapros4: true,
+        title: 'zapros_4'
+    })
+})
+conn.end()
+})
+module.exports = router
